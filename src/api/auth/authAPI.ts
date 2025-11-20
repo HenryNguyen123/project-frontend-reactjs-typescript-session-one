d571713e42966eeb2b67d0fb694757f7f6b56977
Minh Nhat <nhokkudo143@gmail.com>
2025-11-20 22:34:32 +0700

    fix

--- src/api/auth/authAPI.ts
+++ src/api/auth/authAPI.ts
@@ -2,5 +2,5 @@ import axios from '../../setup/axios'
 import type {DataResponse} from "../../redux/slices/auth/logoutSlice"
 
 export const logoutApi = async(data: string)=>  {
-    return await axios.post<DataResponse>(import.meta.env.VITE_REMOVE_JWT_URL, {path: data}) 
+    return await axios.post<DataResponse>(import.meta.env.VITE_REMOVE_JWT_URL, {path: data}, { withCredentials: true }) 
 }
